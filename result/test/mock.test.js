0070bb1afec618b00d8dd8bb47b8cbc9c825d78b
Timothy <tim@t.com>
2021-08-25 15:33:23 +0000

    adding mock test

--- result/test/mock.test.js
+++ result/test/mock.test.js
@@ -26,4 +26,14 @@ describe('mock test 4', () => {
   });
 });
 
+describe('mock test 2', () => {
+  it('unit test 2', () => {
+    expect(true).to.be.true;
+  });
+});
 
+describe('mock test 1', () => {
+  it('unit test 1', () => {
+    expect(true).to.be.true;
+  });
+});
